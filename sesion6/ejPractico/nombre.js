50bd5342286d00769f3770d5e568f9521c6ee590
ddelarosae <ddelarose@uninorte.edu.co>
2022-09-19 10:39:04 -0500

    correcion formulario

--- sesion6/ejPractico/nombre.js
+++ sesion6/ejPractico/nombre.js
@@ -1,10 +1,12 @@
 function validar_formulario(){
-    var usuario = document.getElement.usuario;
-    var correo = document.getElement.correo;
-    var pass = document.getElement.pass;
+    var usuario = document.formRegistro.usuario;
+    var correo = document.formRegistro.correo;
+    var pass = document.formRegistro.pass;
 
-    var usuario_longitud = usuario.value.lenght;
-    if(usuario_longitud == 0 && usuario_longitud<8){
-        alert("Debes ingresar un usuario no vacio con al menos 8 caracteres")
+    var usuario_longitud = usuario.value.length;
+    if(usuario_longitud == 0 || usuario_longitud<8){
+        alert("Debes ingresar un usuario no vacio con al menos 8 caracteres");
+        usuario.focus();
+        return false;
     }
 }
